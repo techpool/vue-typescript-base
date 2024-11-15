a7feba0c71285b8896bf8642f23a08003c5dbd2f
Suryadeep Pal <suryadeep10@gmail.com>
2024-11-15 10:58:11 +0530

    init: base vue project with 2.6

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="app">
+    <TodoList 
+      :todos="todos"
+    />
+  </div>
+</template>
+
+<script>
+import TodoList from './components/TodoList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoList
+  },
+  data() {
+    return {
+      todos: [
+        { id: 1, text: 'Learn Vue' },
+        { id: 2, text: 'Learn Vue Router' },
+        { id: 3, text: 'Learn Vuex' },
+        { id: 4, text: 'Learn Nuxt' }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <h1>Todo List</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.text }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TodoList',
+  props: {
+    todos: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0.5em 0;
+  padding: 0.5em;
+  border: 1px solid #42b983;
+  border-radius: 2px;
+}
+</style>
